86c4f5c2fa091d72e2ab14dfd60870afd7f08125
julai1433 <13121096@tecmor.mx>
2025-10-29 13:18:01 -0700

    Agregar endpoints health/ y api-token-auth/

--- servicio_pacientes/pacientes_api/urls.py
+++ servicio_pacientes/pacientes_api/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pacientes.views import health
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health/', health),
+    path('api-token-auth/', obtain_auth_token),
 ]
